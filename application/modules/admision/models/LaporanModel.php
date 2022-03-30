6888a620ce7b51520ed4db2b9c600798e66da337
ariand <mriskiariandaa@gmail.com>
2022-03-30 17:08:56 +0700

    perbaikan model laporan fix

--- application/modules/admision/models/LaporanModel.php
+++ application/modules/admision/models/LaporanModel.php
@@ -7,18 +7,18 @@ class LaporanModel extends CI_Model
     
    $this->db->select(
     'nama_poliklinik,
-	SUM( IF( MONTH(tgl_berobat), 1 , 0) ) AS jan,
-	SUM( IF( MONTH(tgl_berobat), 2 , 0) ) AS feb,
-	SUM( IF( MONTH(tgl_berobat), 3 , 0) ) AS mar,
-	SUM( IF( MONTH(tgl_berobat), 4 , 0) ) AS apr,
-	SUM( IF( MONTH(tgl_berobat), 5 , 0) ) AS mei,
-	SUM( IF( MONTH(tgl_berobat), 6 , 0) ) AS jun,
-	SUM( IF( MONTH(tgl_berobat), 7 , 0) ) AS jul,
-	SUM( IF( MONTH(tgl_berobat), 8 , 0) ) AS agu,
-	SUM( IF( MONTH(tgl_berobat), 9 , 0) ) AS sep,
-	SUM( IF( MONTH(tgl_berobat), 10 , 0) ) AS okt,
-	SUM( IF( MONTH(tgl_berobat), 11 , 0) ) AS nov,
-	SUM( IF( MONTH(tgl_berobat), 12 , 0) ) AS des'
+	SUM( IF( MONTH(tgl_berobat)=01, 1 , 0) ) AS jan,
+	SUM( IF( MONTH(tgl_berobat)=02, 1 , 0) ) AS feb,
+	SUM( IF( MONTH(tgl_berobat)=03, 1 , 0) ) AS mar,
+	SUM( IF( MONTH(tgl_berobat)=04, 1 , 0) ) AS apr,
+	SUM( IF( MONTH(tgl_berobat)=05, 1 , 0) ) AS mei,
+	SUM( IF( MONTH(tgl_berobat)=06, 1 , 0) ) AS jun,
+	SUM( IF( MONTH(tgl_berobat)=07, 1 , 0) ) AS jul,
+	SUM( IF( MONTH(tgl_berobat)=08, 1 , 0) ) AS agu,
+	SUM( IF( MONTH(tgl_berobat)=09, 1 , 0) ) AS sep,
+	SUM( IF( MONTH(tgl_berobat)=10, 1 , 0) ) AS okt,
+	SUM( IF( MONTH(tgl_berobat)=11, 1 , 0) ) AS nov,
+	SUM( IF( MONTH(tgl_berobat)=12, 1 , 0) ) AS des'
     );
     $this->db->where('year(b.tgl_berobat)',$tahun); 
     $this->db->join('tbl_poliklinik as a','a.id_poliklinik = b.id_poliklinik','left');
